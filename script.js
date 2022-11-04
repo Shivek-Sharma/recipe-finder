27be792cf86ef728001789c7e097e6acb2b1bef9
Shivek <sharmashivek18@gmail.com>
2022-11-04 17:38:12 +0530

    update

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ function getRandomMeal() {
     fetch('https://www.themealdb.com/api/json/v1/1/random.php')
         .then(response => response.json())
         .then(data => {
-            //console.log(data)
+            // console.log(data)
             const meal = data.meals[0];
 
             addMealToDOM(meal);
